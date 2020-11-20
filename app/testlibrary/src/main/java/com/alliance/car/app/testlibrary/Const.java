78f074921b14e56e27f1680bb5ea2a9eda0b1300
binh.phan <binh.phan@lg.com.vn>
2020-11-20 11:52:07 +0700

    library

---
+++ app/testlibrary/src/main/java/com/alliance/car/app/testlibrary/Const.java
@@ -0,0 +1,6 @@
+package com.alliance.car.app.testlibrary;
+
+public class Const {
+    public static final int CONST_1 = 10;
+    public static final int CONST_2 = 20;
+}
---
+++ app/testlibrary/src/main/java/com/alliance/car/app/testlibrary/DateUtils.java
@@ -0,0 +1,7 @@
+package com.alliance.car.app.testlibrary;
+
+public class DateUtils {
+    public int getMonOffYer(){
+        return 12;
+    }
+}
